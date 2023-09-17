de1ba7f6e1984a067095eb47754f0701dbe0a7bb
Dhaval <joshidhaval2002@gmail.com>
2023-09-17 15:29:39 +0530

    Object destrucutred added questionin  question div

--- quiz.js
+++ quiz.js
@@ -7,3 +7,14 @@ const questionObj =
       question:
         "How many pieces of bun are in a Mcdonald's Big Mac?",
     };
+
+//we need this object everytime so lets destructure ,this will helo us in not using questionObj.otions or answers
+
+const {correctAnswer,options,question}=questionObj;
+console.log(options)
+
+// const questionDiv=document.getElementById('question')
+const questionEl=document.getElementById('question')
+questionEl.textContent=question 
+
+// questionDiv.innerHTML='<h1>' + questionObj.question + '</h1>';
