7f617b2d9d89c52e1fbc09582afcd9e2c26060b0
Dhaval <joshidhaval2002@gmail.com>
2023-09-17 15:52:52 +0530

    Added option button inside the option div

--- quiz.js
+++ quiz.js
@@ -11,10 +11,29 @@ const questionObj =
 //we need this object everytime so lets destructure ,this will helo us in not using questionObj.otions or answers
 
 const {correctAnswer,options,question}=questionObj;
-console.log(options)
+// console.log(options)
 
 // const questionDiv=document.getElementById('question')
 const questionEl=document.getElementById('question')
+
+
+// for option div
+const optionEl=document.getElementById('options'); //fetch option div 
+
+const scoreEl=document.getElementById('score'); //fetch score div
+
+
+//creating a button and adding the option inside button
+
+
+
+//MAnipulating DOM
 questionEl.textContent=question 
 
-// questionDiv.innerHTML='<h1>' + questionObj.question + '</h1>';
+//Populating the Options div with the buttons
+
+options.forEach((opt)=>{  //first it will contain opt1 then opt2
+  const btn=document.createElement('button');
+  btn.textContent=opt
+  optionEl.appendChild(btn)
+})
