859e83026f689795a7075cfca493fa50bbcda708
Dhaval <joshidhaval2002@gmail.com>
2023-09-17 15:14:59 +0530

    Quiz game js file question object

---
+++ quiz.js
@@ -0,0 +1,9 @@
+const questionObj = 
+    {
+      category: 'Food & Drink',
+      id: 'qa-1',
+      correctAnswer: 'Three',
+      options: ['Two', 'Three ', 'Four', 'Five'],
+      question:
+        "How many pieces of bun are in a Mcdonald's Big Mac?",
+    };
